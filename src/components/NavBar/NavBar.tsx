a13978672d6fdb14b2272a20ee8e5f0056e2af56
Mario Cia <marioj.cia@gmail.com>
2024-07-14 09:59:56 -0300

    feat: create init NavBar desktop component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+
+const  NavBar = () => {
+ 
+    const StyledTooBar = styled(Toolbar)(() => ({
+       display: "flex",
+       justifyContent: "space-evenly"
+        
+    }))
+
+    return (
+      <>
+        <AppBar position="absolute">
+            <StyledTooBar>
+                <MenuItem>About</MenuItem>
+                <MenuItem>Skills</MenuItem>
+                <MenuItem>Projects</MenuItem>
+            </StyledTooBar>
+        </AppBar>
+      </>
+    )
+  }
+  
+  export default NavBar
